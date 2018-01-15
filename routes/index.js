55964cef91bbeb7d0dc3611e5c0267cbf952b071
Tayo Carvalhal <lucarv@microsoft.com>
2018-01-15 21:23:54 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router = express.Router();
+module.exports = router;
+
+// mqtt without security
+var mqtt = require('mqtt')
+var mclient = mqtt.connect('mqtt://telenet-nbiot.westeurope.cloudapp.azure.com')
+var devices = require('./devices.json')
+
+// raw udp datagrams
+var dgram = require('dgram');
+var userver = dgram.createSocket('udp4');
+const port = 5683;
+
+var coap = require('coap');
+var cserver = coap.createServer()
+
+// azure sdk
+var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
+var Message = require('azure-iot-device').Message;
+var Client = require('azure-iot-device').Client;
+var Protocol = require('azure-iot-device-mqtt').Mqtt;
+// hardcode device for now
+var cs;
+
+var mqtt_msg_counter = 0;
+var udp_msg_counter = 0;
+var lastTemp = 'unknown';
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express', last: lastTemp, mqtt: mqtt_msg_counter, udp: udp_msg_counter });
+
+});
+
+mclient.on('connect', function () {
+    console.log('connect')
+    mclient.subscribe('devices/#')
+})
+
+mclient.on('message', function (topic, message) {
+    // message is Buffer
+    mqtt_msg_counter++;
+
+    var msg = message.toString();
+    var tid = JSON.parse(msg).tid;
+    var timestamp = JSON.parse(msg).tst;
+    console.log('message from: ' + tid + ' at: ' + timestamp)
+
+    for (var i = 0; i < devices.length; i++) {
+        if (devices[i].DeviceID === tid) {
+            cs = devices[i].cs
+
+            var hubMsg = new Message(msg)
+            var aclient = clientFromConnectionString(cs);
+
+            aclient.sendEvent(hubMsg, function (err, res) {
+                if (err)
+                    console.log('Message sending error: ' + err.toString());
+                else {
+                    if (res)
+                        console.log('sent from: ' + tid + ' to hub: ' + JSON.stringify(hubMsg));
+                }
+            })
+        }
+    }
+})
